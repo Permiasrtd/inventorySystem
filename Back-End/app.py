e744a55159d5bd9c5e5fee66ba4074dd79c2dc04
SamuelPurnama <samueljovarenp@gmail.com>
2025-01-04 19:23:07 -0800

    Initial template

---
+++ Back-End/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    return jsonify({"message": "Hello from Flask!"})
+
+@app.route('/api/data', methods=['POST'])
+def post_data():
+    data = request.json
+    return jsonify({"received": data}), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
